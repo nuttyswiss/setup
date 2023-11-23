7ff75d261981617088b229cec64806064425a7ef
Tobias Weingartner <weingart@tepid.org>
2023-11-22 19:16:22 -0800

    Add initial repo handling.

---
+++ init-repos.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+#
+# We assume that we are on an reasonably current Ubuntu LTS system with at
+# least a minimal server based install. Here we install and update all the
+# APT repos that we will need.
+
+if [[ "$(whoami)" != "root" ]]; then 
+        echo "ERROR: not root" 2>& 
+        exit 1
+fi
+
+# Tailscale
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.noarmor.gpg | tee /usr/share/keyrings/tailscale-archive-keyring.gpg > /dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.tailscale-keyring.list | tee /etc/apt/sources.list.d/tailscale.list > /dev/null
+
+# After installing all repositories, update and upgrade.
+sudo apt-get update
+sudo apt-get upgrade
